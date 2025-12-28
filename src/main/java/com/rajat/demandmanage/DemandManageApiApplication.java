1c403a01070a2d02f51e3f6bca34062b894d7838
Rajat Mishra <rajatuohmca2014@gmail.com>
2025-12-28 21:22:20 +0530

    Initial structure for demand api

---
+++ src/main/java/com/rajat/demandmanage/DemandManageApiApplication.java
@@ -0,0 +1,13 @@
+package com.rajat.demandmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemandManageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemandManageApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rajat/demandmanage/DemandManageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajat.demandmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemandManageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
